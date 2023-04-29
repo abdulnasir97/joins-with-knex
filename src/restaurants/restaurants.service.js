9c7eae7ea78dc1818c3b9954b98eff6eec094fa3
Abdul M Nasir <moeid.nasir@gmail.com>
2023-04-29 15:11:44 -0400

    Add files via upload

---
+++ src/restaurants/restaurants.service.js
@@ -0,0 +1,67 @@
+const knex = require("../db/connection");
+
+const tableName = "restaurants";
+
+function averageRating() {
+  return knex(tableName).avg("rating").as("average").first();
+}
+
+function count() {
+  return knex(tableName).count("restaurant_id").first();
+}
+
+function create(newRestaurant) {
+  return knex(tableName)
+    .insert(newRestaurant, "*")
+    .then((createdRecords) => createdRecords[0]);
+}
+
+function destroy(restaurant_id) {
+  return knex(tableName).where({ restaurant_id }).del();
+}
+
+function list() {
+  return knex(tableName)
+    .join("owners", "restaurants.owner_id", "=", "owners.owner_id")
+    .select("restaurant_name", "owner_name", "email")
+    .orderBy("owner_name");
+}
+
+function listAverageRatingByOwner() {
+  return knex(tableName)
+    .join("owners", "restaurants.owner_id", "=", "owners.owner_id")
+    .select("owner_name", knex.raw("AVG(rating) as avg"))
+    .groupBy("owner_name")
+    }
+
+function read(restaurant_id) {
+  return knex(tableName).select("*").where({ restaurant_id }).first();
+}
+
+function readHighestRated() {
+  return knex(tableName)
+    .max("rating")
+    .select("*")
+    .groupBy("rating", "restaurant_id")
+    .orderBy("rating", "desc")
+    .first();
+}
+
+function update(updatedRestaurant) {
+  return knex(tableName)
+    .select("*")
+    .where({ restaurant_id: updatedRestaurant.restaurant_id })
+    .update(updatedRestaurant, "*");
+}
+
+module.exports = {
+  averageRating,
+  count,
+  create,
+  delete: destroy,
+  list,
+  listAverageRatingByOwner,
+  read,
+  readHighestRated,
+  update,
+};
